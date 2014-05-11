597c7eb4586021b0c8241a7f3abc268be6c92956
Perumal <perumal.data@gmail.com>
2014-05-10 20:08:48 -0400

    updated to display legend properly

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ plot3<- function() {
     # combine date and time to Datetime format
     powerdata$DateTime <- as.POSIXct(paste(powerdata$Date, powerdata$Time), format="%d/%m/%Y %H:%M:%S")
     
-    
+   
     #Plot 
     plot(powerdata$Sub_metering_1~powerdata$DateTime,
          type="l", #Specify type of plot as l for line
@@ -22,10 +22,10 @@ plot3<- function() {
     lines(powerdata$Sub_metering_2~powerdata$DateTime, col = "red")
     lines(powerdata$Sub_metering_3~powerdata$DateTime, col = "blue")
     legend("topright",
-           legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+           legend=c("Sub_metering_1    ","Sub_metering_2    ","Sub_metering_3   "),
            col=c("black","red","blue"),
            lty=1,lwd=2)
     #copry to png file
-    dev.copy(png, "plot3.png", height=480, width=480)
+    dev.copy(png, "plot3.png", height=480, width=480)    
     dev.off()
 }
--- plot4.R
+++ plot4.R
@@ -32,7 +32,7 @@ plot4<- function() {
     lines(powerdata$Sub_metering_2~powerdata$DateTime, col = "red")
     lines(powerdata$Sub_metering_3~powerdata$DateTime, col = "blue")
     legend("topright",
-           legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+           legend=c("Sub_metering_1    ","Sub_metering_2    ","Sub_metering_3    "),
            col=c("black","red","blue"),
            lty=1,lwd=2, cex=0.5)
     plot(powerdata$Global_reactive_power~powerdata$DateTime,
